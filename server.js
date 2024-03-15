fe2ca7cd581fa89ba6ae7ef5a553dcefd973bca9
Jordan Harper <jordanharper0@outlook.com>
2024-03-14 20:51:20 -0400

    Added methods: tested with postman using body

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const port = 3000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get("/greeting", (req,res) => {
+    const {first_name, last_name} = req.body;
+    res.send(`Hello, ${first_name} ${last_name}!`);
+});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
